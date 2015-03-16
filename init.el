3742f24178a6400d81797181dc6764d7d836497f
Dusty DeWeese <deweese@google.com>
2015-03-16 15:46:11 -0700

    make sure eieio loads before initializing the purpose package, also make custom.el load before loading any themes

--- init.el
+++ init.el
@@ -1,7 +1,9 @@
 ;; init.el for this setup. Must use Emacs 24
-(require 'org)
-(org-babel-load-file
- (expand-file-name "emacs-init.org" user-emacs-directory))
+
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file)
+
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "emacs-init.org" user-emacs-directory))
