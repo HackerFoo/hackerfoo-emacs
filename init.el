50c1ecaf881717024e4d85372b34d0e2c2893d3b
Dustin DeWeese <deweese@google.com>
2015-03-31 18:07:20 -0700

    make windmove work (sometimes) in org-mode

--- init.el
+++ init.el
@@ -394,6 +394,12 @@
 (winner-mode t)
 (windmove-default-keybindings)
 
+;; Make windmove work in org-mode:
+(add-hook 'org-shiftup-final-hook 'windmove-up)
+(add-hook 'org-shiftleft-final-hook 'windmove-left)
+(add-hook 'org-shiftdown-final-hook 'windmove-down)
+(add-hook 'org-shiftright-final-hook 'windmove-right)
+
 ;;; Imenu:
 (require 'imenu)
 (setq imenu-auto-rescan t)
